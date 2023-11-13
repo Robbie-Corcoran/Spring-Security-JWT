43de71fa0559fbac3fab998499c88d3e4d2e2736
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-13 23:15:35 +0100

    Refactor

--- src/main/java/com/rawtech/Spring/Security/Tutorial/config/SecurityConfig.java
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.rawtech.Spring.Security.Tutorial.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
