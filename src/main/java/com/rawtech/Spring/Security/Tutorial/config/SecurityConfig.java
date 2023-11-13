bd6ef1ec8e80427f5d3042ef85380da5bb3c53d5
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-13 17:12:51 +0100

    Implemented basic controller and SecurityConfig.java. Here we took the defaultSecurityFilterChain from Spring and added our own.

---
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.rawtech.Spring.Security.Tutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+        });
+        http.httpBasic(Customizer.withDefaults());
+        http.formLogin(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+
+}
