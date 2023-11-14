a35d678ef45d7f1eb0dfeb1133283cc0c93e112c
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-14 16:18:23 +0100

    Create the authentication service

--- src/main/java/com/rawtech/Spring/Security/Tutorial/auth/AuthenticationController.java
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/auth/AuthenticationController.java
@@ -12,14 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticationService authenticationService;
+
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-
+        return ResponseEntity.ok(authenticationService.register(request));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
 
     }
 
---
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/auth/AuthenticationService.java
@@ -0,0 +1,18 @@
+package com.rawtech.Spring.Security.Tutorial.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    public AuthenticationResponse register(RegisterRequest request) {
+
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+
+    }
+}
