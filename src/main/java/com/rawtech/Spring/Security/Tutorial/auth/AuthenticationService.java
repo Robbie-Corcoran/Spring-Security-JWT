49ff2223f2365d7ff29c5b39353002ad890b5219
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-14 16:34:18 +0100

    Implement the authenticate method

--- src/main/java/com/rawtech/Spring/Security/Tutorial/auth/AuthenticationService.java
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/auth/AuthenticationService.java
@@ -5,6 +5,9 @@ import com.rawtech.Spring.Security.Tutorial.user.Role;
 import com.rawtech.Spring.Security.Tutorial.user.User;
 import com.rawtech.Spring.Security.Tutorial.user.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +19,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -36,6 +40,13 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
 
     }
 }
