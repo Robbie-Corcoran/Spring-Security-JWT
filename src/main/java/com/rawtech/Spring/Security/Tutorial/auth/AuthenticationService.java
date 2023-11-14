f6e6b8cb76ffbd3d3f4534e84f157cd2431c3c66
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-14 16:30:19 +0100

    mplement the register method

--- src/main/java/com/rawtech/Spring/Security/Tutorial/auth/AuthenticationService.java
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/auth/AuthenticationService.java
@@ -1,6 +1,11 @@
 package com.rawtech.Spring.Security.Tutorial.auth;
 
+import com.rawtech.Spring.Security.Tutorial.config.JwtService;
+import com.rawtech.Spring.Security.Tutorial.user.Role;
+import com.rawtech.Spring.Security.Tutorial.user.User;
+import com.rawtech.Spring.Security.Tutorial.user.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -8,8 +13,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    public AuthenticationResponse register(RegisterRequest request) {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
