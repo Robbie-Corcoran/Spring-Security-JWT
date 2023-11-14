80d343effcf2a7f260cca8ec0ba0ee57abe24975
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-14 16:04:23 +0100

    Create the authentication provider bean

--- src/main/java/com/rawtech/Spring/Security/Tutorial/config/ApplicationConfig.java
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/config/ApplicationConfig.java
@@ -5,9 +5,15 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.swing.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,4 +27,16 @@ public class ApplicationConfig {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found."));
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
