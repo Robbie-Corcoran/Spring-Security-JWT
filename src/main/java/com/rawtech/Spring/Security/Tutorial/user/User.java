dd5b6d1e5f808e5d0ef7eb2430173471866e164b
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-14 12:38:11 +0100

    _user table setup

--- src/main/java/com/rawtech/Spring/Security/Tutorial/user/User.java
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/user/User.java
@@ -1,5 +1,9 @@
 package com.rawtech.Spring.Security.Tutorial.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,12 +13,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
+
     private String password;
 
 }
