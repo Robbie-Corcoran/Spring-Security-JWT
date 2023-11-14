e34cd9bb3ad2760c97c4a9da457c6b84872b582d
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-14 13:34:26 +0100

    Init JWTService.extractAllClaims

--- src/main/java/com/rawtech/Spring/Security/Tutorial/config/JWTService.java
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/config/JWTService.java
@@ -1,5 +1,7 @@
 package com.rawtech.Spring.Security.Tutorial.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,5 +10,9 @@ public class JWTService {
 
     public String extractUsername(String jwtToken) {
         return null;
+    }
+
+    private Claims extractAllClaims (String token) {
+        return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws().getBody();
     }
 }
