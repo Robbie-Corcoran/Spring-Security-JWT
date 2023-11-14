b4362bd4d249b5ee8a91c3b6c25fd07ee7487107
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-14 16:00:09 +0100

    Add the security configuration

--- src/main/java/com/rawtech/Spring/Security/Tutorial/config/SecurityConfig.java
+++ src/main/java/com/rawtech/Spring/Security/Tutorial/config/SecurityConfig.java
@@ -1,22 +1,37 @@
 package com.rawtech.Spring.Security.Tutorial.config;
 
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl) requests.anyRequest()).authenticated();
-        });
-        http.httpBasic(Customizer.withDefaults());
-        http.formLogin(Customizer.withDefaults());
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl) requests.anyRequest()).authenticated())
+            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+//            .httpBasic(Customizer.withDefaults())
+//            .formLogin(Customizer.withDefaults());
         return http.build();
     }
 
